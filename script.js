6a01e8a753be9cbdda958f765fcf85de6deb1d8d
OleseadeObari <emereoleseade@gmail.com>
2023-01-07 17:04:52 +0000

    update

--- script.js
+++ script.js
@@ -1,55 +1,132 @@
 "use strict";
-const btn = document.querySelectorAll(".button");
 
-btn.forEach(function (item) {
-  item.addEventListener("click", function (e) {
-    const style = e.currentTarget.classList;
-    if (style.contains("one")) {
-      btnOne();
-    } else if (style.contains("two")) {
-      btnOne();
-      btnTwo();
-    } else if (style.contains("three")) {
-      btnOne();
-      btnTwo();
-      btnThree();
-    } else if (style.contains("four")) {
-      btnOne();
-      btnTwo();
-      btnThree();
-      btnFour();
-    } else {
-      btnOne();
-      btnTwo();
-      btnThree();
-      btnFour();
-      btnFive();
-    }
-  });
-});
-function btnOne() {
-  one.style.backgroundColor = "hsl(25, 97%, 53%)";
-  one.style.color = "hsl(0, 0%, 100%)";
-  one.style.fontWeight = "bold";
-}
-function btnTwo() {
-  two.style.backgroundColor = "hsl(25, 97%, 53%)";
-  two.style.color = "hsl(0, 0%, 100%)";
-  two.style.fontWeight = "bold";
-}
-function btnThree() {
-  three.style.backgroundColor = "hsl(25, 97%, 53%)";
-  three.style.color = "hsl(0, 0%, 100%)";
-  three.style.fontWeight = "bold";
-}
-function btnFour() {
-  four.style.backgroundColor = "hsl(25, 97%, 53%)";
-  four.style.color = "hsl(0, 0%, 100%)";
-  four.style.fontWeight = "bold";
-}
-function btnFive() {
-  five.style.backgroundColor = "hsl(25, 97%, 53%)";
-  five.style.color = "hsl(0, 0%, 100%)";
-  five.style.fontWeight = "bold";
+const btn1 = document.querySelector(".one");
+
+const btn2 = document.querySelector(".two");
+const btn3 = document.querySelector(".three");
+const btn4 = document.querySelector(".four");
+const btn5 = document.querySelector(".five");
+
+// let state = true;
+let clicked = true;
+let click1 = false;
+let click2 = false;
+let click3 = false;
+let click4 = false;
+let click5 = false;
+function changeCol1() {
+  click1 = true;
+  if (clicked == true && click1 == true) {
+    btn1.style.backgroundColor = "green";
+    console.log("true");
+  } else {
+    btn1.style.backgroundColor = "none";
+  }
 }
 
+// if (state === true) {
+//   btn1.style.backgroundColor = "green";
+//   console.log("state is true");
+// } else {
+//   btn1.style.backgroundColor = "teal";
+//   console.log("state is false");
+// }
+
+function changeCol2() {
+  click2 = true;
+  if (clicked == true && click2 == true) {
+    btn1.style.backgroundColor = "yellow";
+    btn2.style.backgroundColor = "green";
+  } else {
+    // btn1.style.backgroundColor = "gray";
+  }
+}
+
+function changeCol3() {
+  click3 = true;
+  if (clicked == true && click3 == true) {
+    btn1.style.backgroundColor = "yellow";
+    btn2.style.backgroundColor = "yellow";
+    btn3.style.backgroundColor = "green";
+  }
+}
+
+function changeCol4() {
+  click4 = true;
+  if (clicked == true && click4 == true) {
+    btn1.style.backgroundColor = "yellow";
+    btn2.style.backgroundColor = "yellow";
+    btn3.style.backgroundColor = "yellow";
+    btn4.style.backgroundColor = "green";
+  } else {
+    click5 == false;
+    btn5.style.backgroundColor = "red";
+  }
+}
+
+function changeCol5() {
+  click5 = true;
+  if (clicked == true && click5 == true) {
+    btn1.style.backgroundColor = "yellow";
+    btn2.style.backgroundColor = "yellow";
+    btn3.style.backgroundColor = "yellow";
+    btn4.style.backgroundColor = "yellow";
+    btn5.style.backgroundColor = "green";
+  }
+}
+// function btn5() {
+//   btn5.style.backgroundColor = "none";
+//   console.log("hey there");
+// }
+// btn1.style.backgroundColor = !clicked ? "red"  : "none";
+// btn2.style.backgroundColor = !clicked ? "green" : "none";
+
+// function changeCol2(){
+//   if (state)
+// };
+// function changeCol1() {
+//   btn1.clicked === true;
+//   btn1.style.backgroundColor = "red";
+//   btn1.style.fontWeight = "bold";
+//   btn1.style.color = "hsl(0, 0%, 100%)";
+//   console.log("button 1 was clicked");
+// }
+// function changeCol2() {
+//   changeCol1();
+//   btn2.clicked === true;
+//   btn2.style.backgroundColor = "red";
+//   btn2.style.fontWeight = "bold";
+//   btn2.style.color = "hsl(0, 0%, 100%)";
+//   console.log("button 2  was clicked");
+// }
+
+// function changeCol3() {
+//   changeCol1();
+//   changeCol2();
+//   btn3.clicked === true;
+//   btn3.style.backgroundColor = "red";
+//   btn3.style.fontWeight = "bold";
+//   btn3.style.color = "hsl(0, 0%, 100%)";
+//   console.log("button 3  was clicked");
+// }
+// function changeCol4() {
+//   changeCol1();
+//   changeCol2();
+//   changeCol3();
+//   btn4.clicked === true;
+//   btn4.style.backgroundColor = "red";
+//   btn4.style.fontWeight = "bold";
+//   btn4.style.color = "hsl(0, 0%, 100%)";
+//   console.log("button 4 was clicked");
+// }
+// function changeCol5() {
+//   changeCol1();
+//   changeCol2();
+//   changeCol3();
+//   changeCol4();
+//   btn5.clicked === true;
+//   btn5.style.backgroundColor = "red";
+//   btn5.style.fontWeight = "bold";
+//   btn5.style.color = "hsl(0, 0%, 100%)";
+//   console.log("button 5 was clicked");
+// }
